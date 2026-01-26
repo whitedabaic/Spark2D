157bd25f5b742e69a106fc82cc17e095dfc15c79
whitedabaic <1951893824@qq.com>
2026-01-26 16:24:00 +0800

    add tiledmap

---
+++ SPARK_EDITOR/assets/scripts/TestProject/assetDefs.lua
@@ -0,0 +1,21 @@
+AssetDefs = 
+{
+	textures = 
+	{
+		{ name = "collider32", path = "assets/textures/collider32.png", pixel_art = true},
+		{ name = "grass", path = "assets/textures/grass.png", pixel_art = true},
+		{ name = "Atlas-Props", path = "assets/textures/Atlas-Props.png", pixel_art = true},
+
+	},
+
+	music =
+	{
+		
+
+	},
+
+	sound_fx =
+	{
+		
+	}
+}
