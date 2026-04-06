1d8ef9bb224a488d6a4b57bd95fe61c2ac0b1f28
whitedabaic <1951893824@qq.com>
2026-04-06 20:13:14 +0800

    update

--- SPARK_EDITOR/assets/scripts/TestProject/assetDefs.lua
+++ SPARK_EDITOR/assets/scripts/TestProject/assetDefs.lua
@@ -5,7 +5,7 @@ AssetDefs =
 		{ name = "collider32", path = "assets/textures/collider32.png", pixel_art = true},
 		{ name = "grass", path = "assets/textures/grass.png", pixel_art = true},
 		{ name = "Atlas-Props", path = "assets/textures/Atlas-Props.png", pixel_art = true},
-		{ name = "rain", path = "assets/textures/rain.png", pixel_art = true},
+		{ name = "rain", path = "assets/textures/Rain.png", pixel_art = true},
 
 	},
 
@@ -17,6 +17,6 @@ AssetDefs =
 
 	sound_fx =
 	{
-		{ name = "rain", path = "assets/soundfx/rain_loop.mp3"},
+		{ name = "rain", path = "assets/soundfx/rain.mp3"},
 	}
 }
